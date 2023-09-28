74263a11795124c5a5e2d02d062dd3105c9d89ac
Uchijo <kgtnttki.yuta@gmail.com>
2023-09-28 20:14:07 +0900

    検索に引っかからないようにした

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,6 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <title>Walica Clone</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="noindex" />
       </Head>
 
       <div className={styles.container}>
